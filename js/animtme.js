bcafba4ed46e4853c82fb433cc9c8785a26a9ee5
Soumava Pal <soumavapal@gmail.com>
2020-11-11 06:52:25 +0530

    Update animtme.js

--- js/animtme.js
+++ js/animtme.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   $('#start-btn').click(function () {
       $('#animate-me').addClass('animation');
   });
+  $('#start-btnt').click(function () {
+      $('#animate-me').addClass('animation');
+  });
 });
